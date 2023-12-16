7723572ba026b8ffe45f8e014e5a9022d01b6b13
Stefan Probst <stefanprobst@posteo.de>
2023-12-16 17:56:58 +0100

    feat: add issue repro

---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+import type { MetadataRoute } from "next";
+
+/**
+ * If you need more than 50.0000 entries, use `generateSitemaps`.
+ */
+export default function sitemap(): MetadataRoute.Sitemap {
+  return []
+}
